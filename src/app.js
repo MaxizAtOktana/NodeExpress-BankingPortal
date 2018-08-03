4f0d2b3e1e728191284ce67896ef55017b109bcf
Maximiliano Zipitria <maximilianozipitria@MaximilianosMBP.lan>
2018-08-03 14:01:53 -0300

    Completed module 2

--- src/app.js
+++ src/app.js
@@ -13,10 +13,50 @@ app.set("view engine", "ejs");
 const publicDirectoryPath = path.join(__dirname, "public");
 app.use(express.static(publicDirectoryPath));
 
+const accountData = fs.readFileSync(path.join("src", "json", "accounts.json"), {
+  encoding: "UTF-8"
+});
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(path.join("src", "json", "users.json"), {
+  encoding: "UTF-8"
+});
+const users = JSON.parse(userData);
+
 app.get("/", (req, res) => {
-  res.render("index", {
-    title: "Index"
-  });
+  const options = {
+    title: "Account Summary",
+    accounts
+  };
+  res.render("index", options);
+});
+
+app.get("/savings", (req, res) => {
+  const options = {
+    account: accounts.saving
+  };
+  res.render("account", options);
+});
+
+app.get("/checking", (req, res) => {
+  const options = {
+    account: accounts.checking
+  };
+  res.render("account", options);
+});
+
+app.get("/credit", (req, res) => {
+  const options = {
+    account: accounts.credit
+  };
+  res.render("account", options);
+});
+
+app.get("/profile", (req, res) => {
+  const options = {
+    user: users[0]
+  };
+  res.render("profile", options);
 });
 
 app.listen(3000, () => {
